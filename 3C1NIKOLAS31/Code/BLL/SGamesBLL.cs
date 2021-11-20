56a8ebd2e94fd444fadc54d35ca3a1f4d3369f59
Nikolas Amorim <78495481+nikolasamorim@users.noreply.github.com>
2021-11-19 23:37:05 -0300

    Add files via upload

---
+++ 3C1NIKOLAS31/Code/BLL/SGamesBLL.cs
@@ -0,0 +1,40 @@
+﻿
+using _3C1NIKOLAS31.Code.DTO;
+using _3C1NIKOLAS31.Code.DAL;
+using System.Data;
+
+namespace _3C1NIKOLAS31.Code.BLL
+{
+    internal class SGamesBLL
+    {
+        Banco bd = new Banco();
+        string tabela = "savassigames";
+
+
+
+        public void Inserir(SGamesDTO SGamesDTO)
+        {
+
+            string inserir = $"insert into {tabela} values('{ SGamesDTO.Id}','{ SGamesDTO.Produto}','{ SGamesDTO.Preco}')";
+            bd.ExecutarComando(inserir);
+        }
+
+
+        public void Excluir(SGamesDTO SGamesDTO)
+        {
+            string excluir = $"delete from {tabela} where id = '{SGamesDTO.Id}';";
+            bd.ExecutarComando(excluir);
+        }
+
+        public DataTable Listar()
+        {
+            string sql = $"select * from {tabela}; ";
+            return bd.ExecutarConsulta(sql);
+        }
+        public void Editar(SGamesDTO SGamesDTO)
+        {
+            string alterar = $"update {tabela} set produto = '{SGamesDTO.Produto}', preco = '{SGamesDTO.Preco}' where id = '{SGamesDTO.Id}';";
+            bd.ExecutarComando(alterar);
+        }
+    }
+}
---
+++ 3C1NIKOLAS31/Code/DTO/SGamesDTO.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace _3C1NIKOLAS31.Code.DTO
+{
+    internal class SGamesDTO
+    {
+        private string _id;
+        private string _produto;
+        private string _preco;
+
+        public string Id { get => _id; set => _id = value; }
+        public string Produto { get => _produto; set => _produto = value; }
+        public string Preco { get => _preco; set => _preco = value; }
+    }
+}
