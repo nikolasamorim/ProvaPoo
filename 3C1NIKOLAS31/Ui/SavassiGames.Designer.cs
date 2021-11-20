f7572ed862a18162ef58f6f4b545d9d51e1787f9
Nikolas Amorim <78495481+nikolasamorim@users.noreply.github.com>
2021-11-19 23:39:24 -0300

    Add files via upload

---
+++ 3C1NIKOLAS31/Ui/SavassiGames.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace _3C1NIKOLAS31.Ui
+{
+    partial class SavassiGames
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_editar = new System.Windows.Forms.Button();
+            this.btn_excluir = new System.Windows.Forms.Button();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_cadastrar = new System.Windows.Forms.Button();
+            this.dgtlista = new System.Windows.Forms.DataGridView();
+            this.txtpreco = new System.Windows.Forms.TextBox();
+            this.txtproduto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgtlista)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_editar
+            // 
+            this.btn_editar.Location = new System.Drawing.Point(268, 106);
+            this.btn_editar.Name = "btn_editar";
+            this.btn_editar.Size = new System.Drawing.Size(216, 43);
+            this.btn_editar.TabIndex = 39;
+            this.btn_editar.Text = "Editar";
+            this.btn_editar.UseVisualStyleBackColor = true;
+            this.btn_editar.Click += new System.EventHandler(this.btn_editar_Click);
+            // 
+            // btn_excluir
+            // 
+            this.btn_excluir.Location = new System.Drawing.Point(490, 106);
+            this.btn_excluir.Name = "btn_excluir";
+            this.btn_excluir.Size = new System.Drawing.Size(216, 43);
+            this.btn_excluir.TabIndex = 38;
+            this.btn_excluir.Text = "Excluir";
+            this.btn_excluir.UseVisualStyleBackColor = true;
+            this.btn_excluir.Click += new System.EventHandler(this.btn_excluir_Click);
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(96, 27);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(154, 20);
+            this.txtid.TabIndex = 37;
+            this.txtid.TextChanged += new System.EventHandler(this.txtid_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(72, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "ID:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btn_cadastrar
+            // 
+            this.btn_cadastrar.Location = new System.Drawing.Point(46, 106);
+            this.btn_cadastrar.Name = "btn_cadastrar";
+            this.btn_cadastrar.Size = new System.Drawing.Size(216, 43);
+            this.btn_cadastrar.TabIndex = 35;
+            this.btn_cadastrar.Text = "Cadastrar";
+            this.btn_cadastrar.UseVisualStyleBackColor = true;
+            this.btn_cadastrar.Click += new System.EventHandler(this.btn_cadastrar_Click);
+            // 
+            // dgtlista
+            // 
+            this.dgtlista.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgtlista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgtlista.Location = new System.Drawing.Point(29, 189);
+            this.dgtlista.Name = "dgtlista";
+            this.dgtlista.Size = new System.Drawing.Size(748, 230);
+            this.dgtlista.TabIndex = 34;
+            // 
+            // txtpreco
+            // 
+            this.txtpreco.Location = new System.Drawing.Point(536, 27);
+            this.txtpreco.Name = "txtpreco";
+            this.txtpreco.Size = new System.Drawing.Size(154, 20);
+            this.txtpreco.TabIndex = 33;
+            // 
+            // txtproduto
+            // 
+            this.txtproduto.Location = new System.Drawing.Point(316, 27);
+            this.txtproduto.Name = "txtproduto";
+            this.txtproduto.Size = new System.Drawing.Size(154, 20);
+            this.txtproduto.TabIndex = 32;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(488, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Preço:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(257, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Produto:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // SavassiGames
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_editar);
+            this.Controls.Add(this.btn_excluir);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_cadastrar);
+            this.Controls.Add(this.dgtlista);
+            this.Controls.Add(this.txtpreco);
+            this.Controls.Add(this.txtproduto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "SavassiGames";
+            this.Text = "Menu - SavassiGames";
+            this.Load += new System.EventHandler(this.SavassiGames_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgtlista)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_editar;
+        private System.Windows.Forms.Button btn_excluir;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_cadastrar;
+        private System.Windows.Forms.DataGridView dgtlista;
+        private System.Windows.Forms.TextBox txtpreco;
+        private System.Windows.Forms.TextBox txtproduto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ 3C1NIKOLAS31/Ui/SavassiGames.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows.Forms;
+using _3C1NIKOLAS31.Code.BLL;
+using _3C1NIKOLAS31.Code.DTO;
+namespace _3C1NIKOLAS31.Ui
+{
+    public partial class SavassiGames : Form
+    {
+         SGamesBLL SGamesbll = new SGamesBLL();
+         SGamesDTO SGamesdto = new SGamesDTO();
+        public SavassiGames()
+        {
+            InitializeComponent();
+            dgtlista.DataSource = SGamesbll.Listar();
+        }
+
+        private void btn_cadastrar_Click(object sender, EventArgs e)
+        {
+            SGamesdto.Id = txtid.Text;
+            SGamesdto.Produto = txtproduto.Text;
+            SGamesdto.Preco = txtpreco.Text;
+
+            SGamesbll.Inserir(SGamesdto);
+            dgtlista.DataSource = SGamesbll.Listar();
+            MessageBox.Show("Cadastrado!", "Game", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btn_excluir_Click(object sender, EventArgs e)
+        {
+            SGamesdto.Id = txtid.Text;
+            SGamesbll.Excluir(SGamesdto);
+            dgtlista.DataSource = SGamesbll.Listar();
+            MessageBox.Show("Excluido!", "Game", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btn_editar_Click(object sender, EventArgs e)
+        {
+            SGamesdto.Id = txtid.Text;
+            SGamesdto.Produto = txtproduto.Text;
+            SGamesdto.Preco = txtpreco.Text;
+
+
+            SGamesbll.Editar(SGamesdto);
+
+
+            MessageBox.Show("Alterado com sucesso!", "Game", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            dgtlista.DataSource = SGamesbll.Listar();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtid_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SavassiGames_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
